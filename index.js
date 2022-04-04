b20ab250fac44a13970290fd23bf0ef12e2253e2
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2022-04-04 12:30:49 +0300

    remake function

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-function makeSubarrays(arr, num) {
-  const res = new Set();
-  if (num === 0) res.add([]);
-  else {
-    for (const subArr of makeSubarrays(arr, num - 1)) {
-      for (const e of arr) {
-        if (subArr.indexOf(e) !== -1) break;
-        res.add([e, ...subArr].sort((a, b) => a - b));
-      }
-    }
-  }
-  return res;
-}
+const chooseOptimalDistance = (t, k, ls) => {
+  let result = 0;
 
-function chooseOptimalDistance(t, k, ls) {
-  const arrSum = [...makeSubarrays(ls, k)].map((e) =>
-    e.reduce((a, b) => a + b, 0),
-  );
-  const optimal = Math.max(...arrSum.filter((value) => value <= t));
-  if (!arrSum.length || optimal === -Infinity) return null;
-  return optimal;
-}
+  const count = (arr, n, sum) => {
+    if (n) {
+      arr.forEach((e, i) => {
+        count(arr.slice(i + 1), n - 1, sum + e);
+      });
+    } else {
+      if (sum > result && sum <= t) result = sum;
+    }
+  };
+  count(ls, k, 0);
+
+  return result || null;
+};
 
 console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]));
 console.log(chooseOptimalDistance(173, 4, [580, 5, 123, 45, 13, 22]));
 console.log(chooseOptimalDistance(12, 2, [2, 10]));
 console.log(chooseOptimalDistance(163, 3, [50]));
+console.log(
+  chooseOptimalDistance(
+    880,
+    8,
+    [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89],
+  ),
+);
